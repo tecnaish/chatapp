91da88e69096a53715f3eba01c012aab97b3218a
Ishmita Gain <83022298+tecnaish@users.noreply.github.com>
2023-10-24 12:56:45 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import "../styles/auth.css";
 import "../styles/chats.css";
 import "../styles/index.css";
 
-import { ContextProvider } from "@/context/index";
+import { ContextProvider } from "../context";
 
 export default function App({ Component, pageProps }) {
   return (
